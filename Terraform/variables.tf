2c8b91ffefb55f26d9e3c5793bf868c7f6903d79
Arijit9721 <das03285@gmail.com>
2025-12-16 17:32:33 +0530

    provided the proper variables

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -2,9 +2,18 @@ variable "region" {
   type = string
   default = "us-east-1"
 }
-variable "bucket-name" {}
-variable "cloudfront-name" {}
-variable "table_name" {}
+variable "bucket-name" {
+  type = string
+  default = "arijit21-portfolio-website-hosting-bucket"
+}
+variable "cloudfront-name" {
+  type = string
+  default = "portfolio-website-cloudfront"
+}
+variable "table_name" {
+  type = string
+  default = "portfolio-website-dynamodb-table"
+}
 variable "hash_key" {
   type = string
   default = "websites"
@@ -13,4 +22,7 @@ variable "second_key" {
   type = string
   default = "views"
 }
-variable "lambda_name" {}
+variable "lambda_name" {
+  type = string
+  default = "portfolio-website-lambda"
+}
